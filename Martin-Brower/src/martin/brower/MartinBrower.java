b855788aa30e366e0f506b5108863ae11b495cdb
ginadedes <ginad1998@hotmail.com>
2016-11-21 22:39:00 -0500

    deleted CsvReaderClient class because there is a new main method in MartinBrower class to use.

--- Martin-Brower/src/martin/brower/MartinBrower.java
+++ Martin-Brower/src/martin/brower/MartinBrower.java
@@ -5,18 +5,36 @@
  */
 package martin.brower;
 
+
+import java.io.FileNotFoundException;
+import java.util.Map;
+import java.util.*;
+import filereading.*;
 /**
  *
  * @author Gina
  */
 public class MartinBrower {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Hello World!");
+    // TODO code application logic here
+    public static void main(String[] args) throws FileNotFoundException
+    {
+        CsvReader reader = new CsvReader("Bun Manifest 3.csv");
+//        boolean isSummaryRow = reader.isSummaryRow(",,ROUTE:      ,,,,,501,,,,STOP:,,,60,,,STORE:,,,,10903,");
+//        String text = isSummaryRow ? "Found" : "Not Found";
+//        System.out.println(text);
+//        boolean isHeaderRow = reader.isHeaderRow(",,,,,,,,,,,,WRIN,TRAILER POSITION,,,DISPATCH,,,,,,LAST 4 of oLPN");
+//        String text = isHeaderRow ? "Found" : "Not Found";
+//        System.out.println(text);
+          reader.process();
+//          Map<String, String> summaryData  = reader.getSummaryData(",,ROUTE :    ,,,,,589,,,,STOP:,,,30,,,WRIN:,,,,6076,");
+//          for(String v : summaryData.values()) {
+//              System.out.println(v);
+//          }
+          
+//          Map<String, Integer> headerData = reader.getHeaderData(",,,,,,,,,,,,WRIN,TRAILER POSITION,,,DISPATCH,,,,,,LAST 4 of oLPN");
+//          for(String k : headerData.keySet()) {
+//              System.out.printf("<%s,%s>\n", k, headerData.get(k));
+//          }      
     }
-    
 }
